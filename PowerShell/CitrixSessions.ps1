f4a1fb905285e7105bddd5f2299cfea7bdce594a
root <root@WBERF5PWSC2.pgw.local>
2017-06-16 11:07:22 -0400

    Added CitrixSessions.ps1

---
+++ PowerShell/CitrixSessions.ps1
@@ -0,0 +1,44 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$ports = 1494, 2598, 8008
+$sessions = @()
+
+foreach( $connection in Get-NetTCPConnection )
+{
+    if( $ports -contains $connection.RemotePort )
+    {
+        try
+        {
+            $server = [System.Net.Dns]::GetHostEntry($connection.RemoteAddress)
+            if( $server.HostName -like '*ctx*' )
+            {
+                $sessions += $server.HostName
+            }
+        }
+        catch
+        {
+        }
+    }
+}
+
+$response = $null
+
+if( $sessions.Count -gt 0 )
+{
+    $response = [System.Windows.Forms.MessageBox]::Show(
+        $sessions,
+        "Citrix Sessions",
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon]::Information,
+        [System.Windows.Forms.MessageBoxButtons]::OK
+    )
+}
+else
+{
+    $response = [System.Windows.Forms.MessageBox]::Show("No sessions found.","Citrix Sessions",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Error)
+}
+
+#while( $response -eq $null )
+#{
+#    Write-Host waiting...
+#}
